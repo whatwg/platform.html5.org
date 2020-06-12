eba48343d29b2a434512c362164d087fa48d21b0
Michael[tm] Smith <mike@w3.org>
2020-06-12 14:02:37 +0900

    Change checker URL to https://validator.w3.org/nu

--- .html-check.sh
+++ .html-check.sh
@@ -2,7 +2,7 @@
 (for file in index.html specs/index.html history/index.html; do
   echo "Checking $file...";
   curl -s -H "Content-Type: text/html; charset=utf-8" \
-    --data-binary @$file https://checker.html5.org/?out=gnu\&file=$file \
+    --data-binary @$file https://validator.w3.org/nu/?out=gnu\&file=$file \
   | tee -a OUTPUT;
   echo;
 done;)
